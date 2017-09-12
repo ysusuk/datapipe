5b53f0ba25f58bbd96704479f7b48deefab97848
Yuriy Susuk <yuriy.susuk@scout24.com>
2017-09-12 19:04:45 +0200

    recognize -> filter -> evaluate -> print

--- build.sbt
+++ build.sbt
@@ -11,6 +11,7 @@ lazy val root = (project in file(".")).
     libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-actor"  % "2.4.14",
       "com.typesafe.akka" %% "akka-stream" % "2.4.14",
+      "com.typesafe.akka" %% "akka-http"   % "10.0.10",
       scalaTest % Test
     )
   )
--- src/main/scala/Terminal.scala
+++ src/main/scala/Terminal.scala
@@ -1,10 +1,17 @@
 import akka.{ Done, NotUsed }
+import akka.stream.UniformFanOutShape
+import akka.stream.scaladsl.Merge
+import akka.stream.javadsl.FramingTruncation
+import akka.util.ByteString
+import akka.stream.javadsl.Framing
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ClosedShape, scaladsl}
 import scaladsl.{Broadcast, GraphDSL, Flow, Keep, RunnableGraph, Sink, Source}
 import scala.concurrent.Future
 
 object Terminal extends App {
+  import scala.collection.{immutable => immut}
+
   implicit val system = ActorSystem("Cmds-Stream")
   implicit val materializer = ActorMaterializer()
 
@@ -20,13 +27,23 @@ object Terminal extends App {
   case object LS extends Cmd
   case class Cat(fileName: String) extends Cmd
 
+  object Cmd {
+    def apply(str: String): Option[Cmd] = str match {
+      case "ls" =>
+        Some(LS)
+      case cmdStr if str.startsWith("cat") =>
+        Some(Cat(cmdStr.split(" ")(1)))
+      case _ =>
+        None
+    }
+  }
+
   // mix way
-  val ls: Source[LS.type, NotUsed] = Source.single(LS)
-  val cat: Source[Cat, NotUsed] = Source.single(Cat("cv.pdf"))
-  val mix = ls.concatMat(cat)(Keep.both)
+//  val ls: Source[LS.type, NotUsed] = Source.single(LS)
+//  val cat: Source[Cat, NotUsed] = Source.single(Cat("cv.pdf"))
+//  val mix = ls.concatMat(cat)(Keep.both)
 
   // poly way
-  import scala.collection.{immutable => immut}
   val in: Source[Cmd, NotUsed] = Source(immut.Seq[Cmd](LS, Cat("cv.pdf"), LS, Cat("resume.pdf")))
   val out: Sink[String, Future[Done]] = Sink.foreach(println)
 
@@ -55,6 +72,8 @@ object Terminal extends App {
 
   import Evaluator._
   
+  // having a vay to return evaluator based on type for evaluaiton
+  // next step would be to remove case construct
   def eval(cmd: Cmd): String = cmd match {
     case ls: LS.type =>
       Evaluator[LS.type].eval(ls)
@@ -70,7 +89,7 @@ object Terminal extends App {
   // ??? why this is Future[Done]
   val res1 = in
     .map(eval)
-    .runForeach(println)
+//    .runForeach(println)
 
 //  res1.onComplete { _ => system.shutdown }
 
@@ -81,7 +100,7 @@ object Terminal extends App {
     // shutdown condition
     .take(10)
     .to(out)
-    .run
+//    .run
 
   val sumOut: Sink[Int, Future[Int]] = Sink.fold[Int, Int](0)(_ + _)
 
@@ -97,7 +116,63 @@ object Terminal extends App {
     ClosedShape
   })
 
-  g.run
+//  g.run
+
+  val strs: Source[String, NotUsed] = Source(immut.Seq[String]("ls", "cat cv.pdf", "ls", "cat resume.pdf"))
+
+  val terminalGraph = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder => 
+    import GraphDSL.Implicits._
+
+    val recognize: Flow[String, Option[Cmd], NotUsed] = Flow.fromFunction(str => Cmd(str))
+//    val bcast = 
+
+    // a Prism
+    // val recognizeToLsFlow: Flow[String, LS.type, NotUsed] = recognize.collect {
+    //  case Some(ls: LS.type) =>
+    //    ls
+    // }
+
+    val lsFlow: Flow[Option[Cmd], LS.type, NotUsed] = Flow.fromFunction {
+      case Some(ls: LS.type) =>
+        ls
+    }
+
+    val catFlow: Flow[Option[Cmd], Cat, NotUsed] = Flow.fromFunction {
+      case Some(cat: Cat) =>
+        cat
+    }
+
+    val noneFlow: Flow[Option[Cmd], None.type, NotUsed] = Flow.fromFunction {
+      case None =>
+        None
+    }
+
+    val merge = builder.add(Merge[String](3))
+
+    val broadcast = builder.add(Broadcast[Option[Cmd]](3))
+    broadcast.out(0) ~> lsFlow
+    broadcast.out(1) ~> catFlow
+    broadcast.out(2) ~> noneFlow
+
+    
+//    merge.in(0) ~> stub
+//    merge.in(1) ~> stub
+//    merge.in(2) ~> stub
+    
+    // stub to transform everything to str
+    val stub: Flow[Any, String, NotUsed] = Flow.fromFunction(_.toString)
+
+    strs ~> recognize ~> broadcast ~> stub ~> out
+
+    ClosedShape
+  })
+
+  terminalGraph.run
+
+  // Framing.delimiter(ByteString("/n"), 1000, FramingTruncation.ALLOW)
+
 
   // ??? sequence all futures with Future.sequence, and shutdown on complete
+//  system.shutdown
+//  system.awaitTermination
 }
